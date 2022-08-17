1a77532afa9574598f9c7cbcff14898f4ca14774
Alireza <ali1985reza@gmail.com>
2022-08-17 11:04:08 +1000

    database created

--- mySite/app1/models.py
+++ mySite/app1/models.py
@@ -1,3 +1,15 @@
+from tkinter import CASCADE
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    text =      models.CharField(max_length=300)
+    pub_date =  models.DateTimeField()
+
+class Choice(models.Model):
+    question =  models.ForeignKey( Question , on_delete=models.CASCADE)
+    text =      models.CharField(max_length=300)
+    vote =      models.IntegerField(default=0)
+
+
+    
