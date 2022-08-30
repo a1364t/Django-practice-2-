b9812d457c303d6b66f2cbc1da9b77ddd648e4f0
Alireza <ali1985reza@gmail.com>
2022-08-30 11:23:10 +1000

    queries

--- mySite/app1/models.py
+++ mySite/app1/models.py
@@ -6,10 +6,16 @@ class Question(models.Model):
     text =      models.CharField(max_length=300)
     pub_date =  models.DateTimeField()
 
+    def __str__(self):
+        return self.text
+
 class Choice(models.Model):
     question =  models.ForeignKey( Question , on_delete=models.CASCADE)
     text =      models.CharField(max_length=300)
     vote =      models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.text
 
 
     
