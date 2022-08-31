b92ab776b644bfe6c6c7ed103e6e780a32b8a197
Alireza <ali1985reza@gmail.com>
2022-08-31 16:21:25 +1000

    customise admin page

--- locallibSite/locallib/admin.py
+++ locallibSite/locallib/admin.py
@@ -1,10 +1,39 @@
 from django.contrib import admin
 #from locallib import models 
-from .models import Author, Genre, Book, BookInstance
+from . import models
 
 
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(BookInstance)
-admin.site.register(Genre)
+class BooksInstanceInline(admin.StackedInline):  #TabularInline
+    model = models.BookInstance
+    extra = 1
+
+
+@admin.register(models.Author)
+class AuthorAdmin(admin.ModelAdmin):
+    fields = [('first_name', 'last_name'), ('date_of_birth', 'date_of_death')]
+  
+
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    list_filter = ('title', 'author', 'genre')
+    inlines = [BooksInstanceInline]
+
+@admin.register(models.BookInstance)
+class AdminBookInstance(admin.ModelAdmin):
+    fieldsets = (
+        ('Information', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+    
+
+@admin.register(models.Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
 
